8f497416f508644fcb5ab1d50ad7076b65503cea
nkhy-dev-3112 <nkhy.dev@gmail.com>
2024-10-28 15:25:30 +0700

    run lint

--- src/core/models/sort-params.ts
+++ src/core/models/sort-params.ts
@@ -6,7 +6,7 @@ export class SortParams {
   public readonly type: SortType;
 
   constructor(sort: string | undefined, type: SortType | undefined) {
-    this.sort = sort ?? 'created_at';
+    this.sort = sort ?? 'last_update';
     this.type = type ?? SortType.ASC;
   }
 }
