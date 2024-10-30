1553c3631ad4d963d9d2d411d529d749f2775f94
nkhy-dev-3112 <nkhy.dev@gmail.com>
2024-10-30 16:18:54 +0700

    config

--- src/modules/app/config/app.ts
+++ src/modules/app/config/app.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  name: process.env.APP_NAME,
-  port: parseInt(process.env.APP_PORT ?? '80') || 80,
+  name: process.env.APP_NAME_B,
+  port: parseInt(process.env.APP_PORT_B ?? '80') || 80,
   debug: process.env.APP_DEBUG === 'true',
 }));
