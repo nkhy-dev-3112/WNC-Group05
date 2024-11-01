07c34fd149b6d778b8e34c83ec23ae549c07677a
nkhy-dev-3112 <nkhy.dev@gmail.com>
2024-11-01 22:48:29 +0700

    insert dump account

--- src/migrations/1729936532616-users.ts
+++ src/migrations/1729936532616-users.ts
@@ -45,6 +45,12 @@ export class Users1729936532616 implements MigrationInterface {
         columnNames: ['email'],
       }),
     );
+
+    // Insert a user
+    await queryRunner.query(`
+      INSERT INTO users (id, email, password, created_at, updated_at)
+      VALUES ('unique-user-id', 'user@example.com', '12345678', NOW(), NOW())
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
